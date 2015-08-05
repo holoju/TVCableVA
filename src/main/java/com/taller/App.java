e50b048081c81e0e2f9dd5128ea8b95f38818ff6
horacio <horacio.lj@gmail.com>
2015-08-04 23:14:40 -0400

    O nuevo mensaje

--- src/main/java/com/taller/App.java
+++ src/main/java/com/taller/App.java
@@ -2,5 +2,6 @@ package com.taller;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hola OTTY" );
+		System.out.println( "esta sera la forma de trabajar" );
     }
 }
