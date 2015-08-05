522f9e6cbd1a6ef773a0f9930ea563dbf80eb246
horacio <horacio.lj@gmail.com>
2015-08-05 19:52:23 -0400

    O Hola mundo con Spark

--- src/main/java/com/taller/App.java
+++ src/main/java/com/taller/App.java
@@ -1,7 +1,25 @@
 package com.taller;
+
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hola OTTY" );
-		System.out.println( "esta sera la forma de trabajar" );
+		System.out.println("esta sera la forma de trabajar");
+
+        Route ruta = new Route("/") {
+            public Object handle(Request request, Response response){
+                return "Hola mundo desde spark";
+            }
+        };
+
+        Spark.get(ruta);
+
     }
+
+
 }
