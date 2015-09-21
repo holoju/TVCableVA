b25aeca9d4e02cf1fa510e9326dd5f7043edab40
horacio <horacio.lj@gmail.com>
2015-09-20 23:38:30 -0400

    M Integracion con MongoDB M un ejemplo de login

--- src/main/java/com/taller/Main.java
+++ src/main/java/com/taller/Main.java
@@ -1,14 +1,21 @@
 package com.taller;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import freemarker.template.Configuration;
-import freemarker.template.Template;
 import freemarker.template.Version;
-
+import org.bson.Document;
+import org.bson.codecs.DocumentCodec;
+import org.bson.codecs.EncoderContext;
+import org.bson.conversions.Bson;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriter;
+import org.bson.json.JsonWriterSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import spark.*;
+import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
-
 
 import java.io.StringWriter;
 import java.util.HashMap;
@@ -27,11 +34,112 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Configuracion para la carpeta de archivos estaticos
+        // Configuracion para la carpeta de archivos estaticos es la carpeta resources
         staticFileLocation("/");
 
         final Configuration configuracion = new Configuration(new Version("2.3.23")); //configuracion para los templates
         configuracion.setClassForTemplateLoading(Main.class, "/"); //direcciona la carpeta de vistas
+
+        FreeMarkerEngine fremarkerConfiguracion = new FreeMarkerEngine(configuracion);
+
+
+        //configuracion para MongoDB
+        //aqui puede ir MongoClientOptions para personalizar las opciones d coneccion
+        MongoClient clienteBase = new MongoClient("localhost", 27017);
+        MongoDatabase db = clienteBase.getDatabase("test");
+        MongoCollection<Document> coleccion = db.getCollection("names");  //trae documentos
+        //MongoCollection<BsonDocument> collection = db.getCollection("names",BsonDocument.class);  //trae bsondocuments que son safe type
+
+
+        Document horacio = new Document("nombre", "Horacio Lopez Justiniano")
+                .append("edad", 25)
+                .append("usuario", "holoju")
+                .append("password", "horacio");
+
+
+        //coleccion.insertOne(horacio); //inserta un usuario a la base de datos
+
+        inesertaUsuario();
+
+
+        //probar que esta funcionando
+        get("/ping", (req, res) -> "pong\n");
+        get("/test", (req, res) -> "La aplicacion funciona correctamente");
+        get("/saludar/:algo", (request, response) -> "Hola " + request.params(":algo"));
+
+
+        //primer pagina o inicio
+        get("/", (request, response) -> {
+            return new ModelAndView(null, "/pages/examples/login.html");
+        }, fremarkerConfiguracion);
+
+
+        post("/login", ((request, response) -> {
+            //se recupera los datos del formulario
+            String user = request.queryParams("usuario");
+            String pass = request.queryParams("password");
+            if (user.isEmpty() || pass.isEmpty()) {
+                log.debug("usuario o password son vacios");
+                response.redirect("/");
+            } else {
+                //validar el password y el usuario con la base
+                Bson filtro = new Document("usuario", user).append("password", pass);
+                Document usuarioEncontrado = coleccion.find(filtro).first();
+
+
+                if (usuarioEncontrado != null) {
+                    //redireccionar a la pagina principal
+                    System.out.println("Usuario encontrado");
+                    printJson(usuarioEncontrado);
+                } else {
+                    //mandar mensaje de error a la pagina de login
+                    System.out.println("no hay el usuario");
+                }
+
+            }
+            return "logueado correctamente " + user;
+        }));
+
+
+        //ejemplo para redireccionar
+        get("/index", (request, response) -> {
+            response.redirect("/");
+            halt();
+            return null;
+        });
+
+
+        //otros ejemplos, despues borrar
+        get("/hello", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+            attributes.put("name", "Horacio Lopez Justiniano");
+
+            // The hello.ftl file is located in directory:
+            // src/test/resources/spark/template/freemarker
+            return new ModelAndView(attributes, "hello.html");
+        }, fremarkerConfiguracion);
+
+        get("/template/:name", (request, response) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("name", request.params(":name"));
+            return new ModelAndView(model, "hello.ftl");
+        }, new FreeMarkerEngine(configuracion));
+
+
+        //para cualquier otra cosa redireccionar a la pagina 404
+        get(":otraCosa", (request, response) -> {
+            //0log.error("404 pagina no encontrada!!");
+            HashMap<String, Object> datos = new HashMap<>();
+            datos.put("pagina", request.params(":otraCosa"));
+            return new ModelAndView(datos, "/pages/examples/404.html");
+        }, fremarkerConfiguracion);
+
+
+
+
+
+
 
 
 
@@ -82,39 +190,18 @@ public class Main {
         });
 */
 
+    }
 
-        get("/ping", (req, res) -> "pong\n");
-        get("/test", (req, res) -> "La aplicacion funciona correctamente");
+    private static void inesertaUsuario() {
 
-        get("/saludar/:algo", (request, response) -> "Hola " + request.params(":algo"));
+    }
 
-        post("/loguear", ((request, response) -> {
-            String user = request.queryParams("user");
-            String pass = request.queryParams("pass");
-            return "logueado correctamente " + user;
-        }));
 
-        get("/hello", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-            attributes.put("name", "Horacio Lopez Justiniano");
-
-            // The hello.ftl file is located in directory:
-            // src/test/resources/spark/template/freemarker
-            return new ModelAndView(attributes, "hello.html");
-        }, new FreeMarkerEngine(configuracion));
-
-        get("/template/:name", (request, response) -> {
-            HashMap<String, Object> model = new HashMap<>();
-            model.put("name", request.params(":name"));
-            return new ModelAndView(model, "hello.ftl");
-        }, new FreeMarkerEngine(configuracion));
-
-        get("/index", (request, response) -> {
-            HashMap<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "index.html");
-        }, new FreeMarkerEngine(configuracion));
-
+    public static void printJson(Document document) {
+        JsonWriter jsonWriter = new JsonWriter(new StringWriter(), new JsonWriterSettings(JsonMode.SHELL, true));
+        new DocumentCodec().encode(jsonWriter, document, EncoderContext.builder().isEncodingCollectibleDocument(true).build());
+        System.out.println(jsonWriter.getWriter());
+        System.out.flush();
     }
 
 }
